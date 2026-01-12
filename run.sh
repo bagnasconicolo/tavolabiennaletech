3119fd483d0976e2de8e0a20622d388d2c6e579b
Nicolò Bagnasco <bagnasco@Nicolos-MacBook-Air-2.local>
2026-01-12 16:59:33 +0100

    Aggiornamento automatico dei dati

--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@ echo "Generating ./html/index.html"
 python v2.py \
   --api-url "https://script.google.com/macros/s/AKfycbwaaePQ5ou6wDggvkpsPvYhYHrZ7W9TpM7QZeVhFhgmjzDl6liP26R3rxaXHlKmJLQidg/exec" \
   --output ./html/index.html \
-  --title "Tavola periodica – campioni" 
+  --title "Tracker progressi - Piccolo Museo della Tavola Periodica @ Biennale Tech 2026 – campioni" 
 echo "Generated ./html/index.html"
 
+git add .
+git commit -m "Aggiornamento automatico dei dati"
+git push
