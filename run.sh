f732c50f45803c0cf3df16403b48f7bfdec8dbd4
Nicolò Bagnasco <bagnasco@Nicolos-MacBook-Air-2.local>
2026-01-12 17:01:04 +0100

    Aggiornamento automatico dei dati

--- run.sh
+++ run.sh
@@ -1,10 +1,14 @@
-echo "Generating ./html/index.html"
+echo "👀 Generating ./html/index.html"
 python v2.py \
   --api-url "https://script.google.com/macros/s/AKfycbwaaePQ5ou6wDggvkpsPvYhYHrZ7W9TpM7QZeVhFhgmjzDl6liP26R3rxaXHlKmJLQidg/exec" \
   --output ./html/index.html \
   --title "Tracker progressi - Piccolo Museo della Tavola Periodica @ Biennale Tech 2026 – campioni" 
-echo "Generated ./html/index.html"
-
+echo "✅ Generated ./html/index.html"
+echo "✅ Updating remote repository"
 git add .
+echo "✅ Committing and pushing changes to remote repository"
 git commit -m "Aggiornamento automatico dei dati"
+echo "✅ Pushing changes to remote repository"
 git push
+echo "✅ Aggiornato il repository remoto"
+echo "🚀 Done!"
