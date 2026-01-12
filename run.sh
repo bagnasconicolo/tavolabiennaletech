b0c20b079476426b14e92f0a280b565c65628c80
Nicolò Bagnasco <bagnasco@Nicolos-MacBook-Air-2.local>
2026-01-12 16:53:36 +0100

    updates

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "Generating ./html/index.html"
+python v2.py \
+  --api-url "https://script.google.com/macros/s/AKfycbwaaePQ5ou6wDggvkpsPvYhYHrZ7W9TpM7QZeVhFhgmjzDl6liP26R3rxaXHlKmJLQidg/exec" \
+  --output ./html/index.html \
+  --title "Tavola periodica – campioni" 
+echo "Generated ./html/index.html"
+
